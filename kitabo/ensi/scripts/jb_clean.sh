368ffe2858bc39840c3d6e2a0cf45ea7009f70af
abikesa <abikesa.sh@gmail.com>
2025-04-06 16:51:44 -0400

    beautiful error

---
+++ kitabo/ensi/scripts/jb_clean.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Define target directories
+dirs=(
+  pdfs
+  figures
+  media
+  testbin
+  nis
+  myhtml
+  dedication
+)
+
+# Create directories under _build/html
+for d in "${dirs[@]}"; do
+  mkdir -p "_build/html/$d"
+  cp -r "$d/"* "_build/html/$d/" 2>/dev/null
+done
+
